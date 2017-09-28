5e4fe2556e34666db7a5d8f04e86e32b111f8be3
insaider <insaider@bk.ru>
2017-09-28 15:14:01 +1000

    CollectionPro_Iterator_5.1.3

---
+++ Collections_Pro/Iterator/5.1.3/src/test/java/ru.skorikov/SimpleNumberIteratorTest.java
@@ -0,0 +1,46 @@
+package ru.skorikov;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @ author: Alex_Skorikov.
+ * @ date: 28.09.17
+ * @ version: java_kurs_standart
+ * 5.1.3. Создать итератор простые числа.
+ * Класс тест.
+ */
+public class SimpleNumberIteratorTest {
+    /**
+     * Проверим наличие простого числа в массиве.
+     */
+    @Test
+    public void whenSimpleNumberInArray() {
+        int[] num = new int[]{1, 4, 13};
+        SimpleNumberIterator itr = new SimpleNumberIterator(num);
+
+        itr.next();
+        itr.next();
+        int test = (int) itr.next();
+
+        Assert.assertEquals(test, 13);
+    }
+
+    /**
+     * Проверим отсутствие простого числа в массиве.
+     */
+    @Test
+    public void whenSimpleNumberOutArray() {
+        int[] num = new int[]{1, 4, 6};
+        SimpleNumberIterator itr = new SimpleNumberIterator(num);
+
+        boolean test = itr.hasNext();
+
+        Assert.assertFalse(test);
+    }
+
+}
