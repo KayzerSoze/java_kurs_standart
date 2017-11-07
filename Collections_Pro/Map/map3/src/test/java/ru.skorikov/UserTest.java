f3dd275c9c3505b481aef75c91c119d82a4d9509
Alex <insaider@bk.ru>
2017-11-07 17:47:19 +1000

    CollectionPro_Map_3. Перекрывать hashCode

---
+++ Collections_Pro/Map/map3/src/test/java/ru.skorikov/UserTest.java
@@ -0,0 +1,62 @@
+package ru.skorikov;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @ author: Alex_Skorikov.
+ * @ date: 04.11.17
+ * @ version: java_kurs_standart
+ */
+public class UserTest {
+
+    /**
+     * Пробуем получить имя.
+     *
+     * @throws Exception исключение.
+     */
+    @Test
+    public void whenGetNameThenReturnName() throws Exception {
+        User user = new User("Name", 1, 1, 12, 1990);
+        Assert.assertEquals("Name", user.getName());
+    }
+
+    /**
+     * Пробуем задать имя.
+     *
+     * @throws Exception исключение.
+     */
+    @Test
+    public void whenSetNameThenReturnName() throws Exception {
+        User user = new User("Name", 1, 1, 12, 1990);
+        user.setName("Name2");
+        Assert.assertEquals("Name2", user.getName());
+    }
+
+    /**
+     * Пробуем получить детей.
+     *
+     * @throws Exception исключение.
+     */
+    @Test
+    public void whenGetChildrenThenReturnChildren() throws Exception {
+        User user = new User("Name", 3, 1, 12, 1990);
+        Assert.assertEquals(3, user.getChildren());
+    }
+
+    /**
+     * Пробуем задать детей.
+     *
+     * @throws Exception исключение.
+     */
+    @Test
+    public void whenSetChildrenThenReturnNewChildren() throws Exception {
+        User user = new User("Name", 1, 1, 12, 1990);
+        user.setChildren(4);
+        Assert.assertEquals(4, user.getChildren());
+    }
+
+}
