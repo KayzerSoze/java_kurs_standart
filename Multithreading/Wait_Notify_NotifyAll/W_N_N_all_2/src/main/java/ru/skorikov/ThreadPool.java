e919e9f48af2ad8978552e31a32250e0888593f4
Alex <insaider@bk.ru>
2018-02-04 13:38:54 +1000

    small change

---
+++ Multithreading/Wait_Notify_NotifyAll/W_N_N_all_2/src/main/java/ru/skorikov/ThreadPool.java
@@ -0,0 +1,11 @@
+package ru.skorikov;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @ author: Alex_Skorikov.
+ * @ date: 22.01.18
+ * @ version: java_kurs_standart
+ */
+public class ThreadPool {
+}
